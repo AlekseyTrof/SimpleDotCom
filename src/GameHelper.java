474677644a50f64d4badfd8affc45902dcbd3e81
AlekseyTrof <alekcey.trofimov.99@gmail.com>
2024-11-29 13:16:12 +0300

    added change

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -3,16 +3,16 @@ import java.io.*;
 public class GameHelper {
     public String getUserInput(String prompt) {
         String inputLine = null;
-        System.out.println(prompt + " ");
+        System.out.println(prompt);
         try {
             BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
             inputLine = is.readLine();
-            if (inputLine.length() == 0) {
+            if (inputLine.isEmpty()) {
                 return null;
             }
         }
         catch (IOException e) {
-            System.out.println("IOException: " + e);
+            System.out.println("IOException:" + e);
         }
         return inputLine;
     }
