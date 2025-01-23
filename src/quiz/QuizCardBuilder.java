2fd46b23fcd3f5011e7860efe3cafe4476d99348
alex <alekcey.trofimov.99@mail.ru>
2025-01-23 14:44:23 +0300

    added package quiz

---
+++ src/quiz/QuizCardBuilder.java
@@ -0,0 +1,137 @@
+package quiz;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class QuizCardBuilder {
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private JFrame frame;
+
+    public static void main(String[] args) {
+        QuizCardBuilder builder = new QuizCardBuilder();
+        builder.go();
+    }
+
+    public void go() {
+        frame = new JFrame("Quiz Card Builder");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+        question = new JTextArea(6, 20);
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+        question.setFont(bigFont);
+
+        JScrollPane qScroller = new JScrollPane(question);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        answer = new JTextArea(6, 20);
+        answer.setLineWrap(true);
+        answer.setWrapStyleWord(true);
+        answer.setFont(bigFont);
+
+        JScrollPane aScroller = new JScrollPane(answer);
+        aScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        aScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JButton nexButton = new JButton("Next Card");
+
+        cardList = new ArrayList<>();
+
+        JLabel qLabel = new JLabel("Question");
+        JLabel aLabel = new JLabel("Answer");
+
+        mainPanel.add(qLabel);
+        mainPanel.add(qScroller);
+        mainPanel.add(aLabel);
+        mainPanel.add(aScroller);
+        mainPanel.add(nexButton);
+        nexButton.addActionListener(new NextCardListener());
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        newMenuItem.addActionListener(new NewMenuListener());
+        saveMenuItem.addActionListener(new SaveMenuListener());
+
+        fileMenu.add(newMenuItem);
+        fileMenu.add(saveMenuItem);
+        menuBar.add(fileMenu);
+
+        frame.setJMenuBar(menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+    }
+
+    public class NextCardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(question.getText(), answer.getText());
+            cardList.add(card);
+            clearCard();
+        }
+    }
+
+    public class SaveMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(question.getText(), answer.getText());
+            cardList.add(card);
+
+            /*
+            Вызывается диалоговое окно, и программа останавливаенся на этой строке,
+             пока пользователь не выберет меню Save. Всю навигацию,
+             выбор файла и т.д. за нас выполняет класс JFileChooser.
+             */
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.showSaveDialog(frame);
+            saveFile(fileSave.getSelectedFile());
+        }
+    }
+
+    public class NewMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cardList.clear();
+            clearCard();
+        }
+    }
+
+    private void clearCard() {
+        question.setText("");
+        answer.setText("");
+        question.requestFocus();
+    }
+
+    /*
+    Метод, который непосредственно записывает файл(вызывается обработчиком событий класса Save-MenuListener).
+    Аргумент - это обьект File, который сохраняется пользователем.
+     */
+    private void saveFile(File file) {
+        try {
+            //мы соединяем буффер с новым файлБуффер для болеее эффективной записи
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            //Пробегаем через список с карточками и записываем их по одной на строчку, разделяя вопрос и ответ символом "\"
+            // и в конце добавляя символ новой строки "n".
+            for (QuizCard card : cardList) {
+                writer.write(card.getQuestion() + "/");
+                writer.write(card.getAnswer() + "\n");
+            }
+            writer.close();
+        } catch (IOException exception) {
+            System.out.println("couldn`t write the cardList out");
+            exception.printStackTrace();
+        }
+    }
+}
